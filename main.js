8bbe4aeda528eb9632bc2ca605e3b47a10e35c80
Keizzmannbanjo <keizzmannbanjo@gmail.com>
2021-09-07 16:19:21 +0100

    Enabled localstorage usage and data display to UI

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-let toggleIcon = document.querySelector("#toggle-icon")
+// ?        VARIABLES DECRALATION HERE
+let toggleIcon = document.querySelector("#toggle-icon"),
+    todoList = document.querySelector(".todos"),
+    todoInput = document.querySelector(".todo_input")
 
+// ?    Retrieves or creates todos from local storage of browser
+let todos = JSON.parse(localStorage.getItem("todos") || "[]")
+
+// ?    Retrieves ID count of todos
+let idCount = JSON.parse(localStorage.getItem("count") || "0")
+
+// ?    EVENTS LISTENERS HERE
+
+// ? Handles todo form submission
+todoInput.addEventListener("keypress", function (e) {
+    let val = todoInput.value
+    if (e.keyCode == 13) {
+        e.preventDefault()
+        if (val[0] == " " || val == "") {
+            alert("Sorry, task cannot begin with space")
+        } else {
+            createTodo(val)
+        }
+    }
+})
+
+// ?    EVENT HANDLERS/ FUNCTIONS HERE
+// ? Toggles dark mode
 toggleIcon.onclick = function () {
     document.body.classList.toggle("dark_mode")
     let x = window.matchMedia("(max-width: 750px)")
@@ -21,3 +47,49 @@ toggleIcon.onclick = function () {
         }
     }
 }
+
+// ? Creates new todo to localstorage
+function createTodo(text) {
+    todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    idCount = JSON.parse(localStorage.getItem("count") || "0")
+    let todo = {
+        id: idCount == 0 ? 1 : idCount + 1,
+        text: text,
+        isCompleted: false
+    }
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos))
+    localStorage.setItem("count", JSON.stringify(idCount + 1))
+    fillTodoList(todos)
+    todoInput.value = ""
+}
+
+// ? Fills the TODO UI
+function fillTodoList() {
+    todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    todoList.innerHTML = ""
+    todos.forEach((item) => {
+        console.log(item)
+        let todo = document.createElement("li"),
+            circle_wrap = document.createElement("div"),
+            circle = document.createElement("div"),
+            todoText = document.createElement("span"),
+            delete_link = document.createElement("a"),
+            delete_icon = document.createElement("img")
+        delete_link.setAttribute("href", "#")
+        delete_link.setAttribute("class", "todo_delete")
+        delete_icon.setAttribute("src", "./images/icon-cross.svg")
+        circle.setAttribute("class", "circle")
+        circle_wrap.setAttribute("class", "circle-wrap")
+        todoText.setAttribute("class", "todo_text")
+        todoText.innerHTML = item.text
+        delete_link.append(delete_icon)
+        circle_wrap.append(circle)
+        todo.append(circle_wrap)
+        todo.append(todoText)
+        todo.append(delete_link)
+        todoList.append(todo)
+    })
+}
+
+fillTodoList()
