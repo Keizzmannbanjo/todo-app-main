ae1e6a89227f7d002d12694ef9ee10b1d9de1a8d
Keizzmannbanjo <keizzmannbanjo@gmail.com>
2021-09-09 13:48:43 +0100

    Completed project

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // ?        VARIABLES DECRALATION HERE
 let toggleIcon = document.querySelector("#toggle-icon"),
     todoList = document.querySelector(".todos"),
-    todoInput = document.querySelector(".todo_input")
+    todoInput = document.querySelector(".todo_input"),
+    footerLinks = document.querySelectorAll("#footer_links a"),
+    clearCompleted = document.querySelector("#clear-completed"),
+    checkInputs
 
 // ?    Retrieves or creates todos from local storage of browser
 let todos = JSON.parse(localStorage.getItem("todos") || "[]")
@@ -51,49 +54,156 @@ toggleIcon.onclick = function () {
 // ? Creates new todo to localstorage
 function createTodo(text) {
     todos = JSON.parse(localStorage.getItem("todos") || "[]")
-    idCount = JSON.parse(localStorage.getItem("count") || "0")
+    idCount = JSON.parse(localStorage.getItem("ID") || "0")
+    count = JSON.parse(localStorage.getItem("count") || "0")
     let todo = {
         id: idCount == 0 ? 1 : idCount + 1,
         text: text,
         isCompleted: false
     }
+    idCount = todo.id
     todos.push(todo)
     localStorage.setItem("todos", JSON.stringify(todos))
-    localStorage.setItem("count", JSON.stringify(idCount + 1))
+    localStorage.setItem("count", JSON.stringify(todos.length))
+    localStorage.setItem("ID", JSON.stringify(idCount))
     fillTodoList(todos)
     todoInput.value = ""
 }
 
-// ? Fills the TODO UI
-function fillTodoList() {
+// ? Handles footer links click
+footerLinks.forEach((link) => link.addEventListener("click", displayClickedItems))
+
+// ? Handles clear completed click
+clearCompleted.addEventListener("click", function (e) {
+    e.preventDefault()
     todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    todos = todos.filter((item) => item.isCompleted != true)
+    localStorage.setItem("todos", JSON.stringify(todos))
+    localStorage.setItem("count", JSON.stringify(todos.length))
+    fillTodoList()
+})
+
+// ? Fills the TODO UI
+function fillTodoList(action) {
+    todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    let uncompleted = 0
     todoList.innerHTML = ""
+    if (action == "active") {
+        todos = todos.filter((item) => item.isCompleted == false)
+    } else if (action == "completed") {
+        todos = todos.filter((item) => item.isCompleted == true)
+    }
     if (todos.length == 0) {
-        todoList.innerHTML = "<h1 class='text-center'> Todo list is empty</h1>"
+        todoList.innerHTML = "<h1 class='text-center'> This list is empty</h1>"
     } else {
         todos.forEach((item) => {
-            console.log(item)
             let todo = document.createElement("li"),
-                circle_wrap = document.createElement("div"),
+                circle_wrap = document.createElement("label"),
                 circle = document.createElement("div"),
                 todoText = document.createElement("span"),
                 delete_link = document.createElement("a"),
-                delete_icon = document.createElement("img")
+                delete_icon = document.createElement("img"),
+                checkInput = document.createElement("input")
+            checkInput.setAttribute("id", String(item.id) + "check")
+            checkInput.setAttribute("type", "checkbox")
+            if (item.isCompleted) {
+                checkInput.checked = true
+                circle.classList.add("completed")
+                circle.style.backgroundImage = "url('./images/icon-check.svg')"
+                circle.style.backgroundPosition = "center"
+                circle.style.backgroundRepeat = "no-repeat"
+                circle_wrap.style.backgroundImage = getComputedStyle(document.documentElement).getPropertyValue(
+                    "--color-blue-grad"
+                )
+                circle_wrap.addEventListener("mouseenter", handleCompleteHover)
+            } else {
+                uncompleted += 1
+                checkInput.checked = false
+                circle.style.backgroundImage = ""
+                circle_wrap.style.backgroundImage = ""
+                circle_wrap.removeEventListener("mouseenter", handleCompleteHover)
+            }
+            if (uncompleted > 0) {
+                document.querySelector(".todo_count").innerHTML = uncompleted + " items left"
+            }
+            circle_wrap.append(circle)
+
+            circle_wrap.setAttribute("for", String(item.id) + "check")
             delete_link.setAttribute("href", "#")
             delete_link.setAttribute("class", "todo_delete")
+            delete_link.setAttribute("id", String(item.id))
+            delete_icon.setAttribute("id", String(item.id))
             delete_icon.setAttribute("src", "./images/icon-cross.svg")
             circle.setAttribute("class", "circle")
             circle_wrap.setAttribute("class", "circle-wrap")
             todoText.setAttribute("class", "todo_text")
             todoText.innerHTML = item.text
             delete_link.append(delete_icon)
-            circle_wrap.append(circle)
+            delete_icon.addEventListener("click", handleDelete)
             todo.append(circle_wrap)
             todo.append(todoText)
             todo.append(delete_link)
+            todo.append(checkInput)
+            checkInput.style.display = "none"
             todoList.append(todo)
         })
     }
+    checkInputs = document.querySelectorAll("input[type='checkbox']")
+    checkInputs.forEach((item) => item.addEventListener("click", handleCompleted))
+}
+
+function handleCompleted(e) {
+    let todoID = parseInt(e.target.id)
+    todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    if (todos.length != 0) {
+        todos.forEach((item) => {
+            if (item.id == todoID) {
+                if (e.target.checked) {
+                    item.isCompleted = true
+                } else {
+                    item.isCompleted = false
+                }
+            }
+        })
+    }
+    localStorage.setItem("todos", JSON.stringify(todos))
+    fillTodoList()
+}
+
+// ? Handle hover events on completed items
+function handleCompleteHover(e) {
+    let child = e.target.children[0]
+    child.style.backgroundColor = "transparent"
+}
+
+// ? Handle deletion link click
+function handleDelete(e) {
+    e.preventDefault()
+    todos = JSON.parse(localStorage.getItem("todos") || "[]")
+    todos = todos.filter((item) => item.id != e.target.id)
+    localStorage.setItem("todos", JSON.stringify(todos))
+    localStorage.setItem("count", JSON.stringify(todos.length))
+    fillTodoList()
+}
+
+// ? Display items based on footer link clicked
+function displayClickedItems(e) {
+    e.preventDefault()
+    footerLinks.forEach((item) => item.classList.remove("active"))
+    let action
+    switch (e.target.dataset.action) {
+        case "active":
+            action = "active"
+            break
+        case "completed":
+            action = "completed"
+            break
+        default:
+            action = "all"
+            break
+    }
+    e.target.classList.add("active")
+    fillTodoList(action)
 }
 
 fillTodoList()
